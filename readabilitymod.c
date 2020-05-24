a294a44b606a7f68751ecbe90fea9a0107c9ccdf
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2020-05-24 21:58:39 +0530

    Add files via upload

---
+++ readabilitymod.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+#include<math.h>
+
+//Program to compute the english us grade level as diefined by Coleman-Liau.
+int main(void)
+{
+    int length;
+    string ext = get_string("Text : ");
+    length = strlen(ext);
+    float totletter = 0, totword = 1, totsen = 0;
+    int subword = 0, subsen = 0, subsen2 = 0, redsen = 0;
+    for (int i = 0; i < length; i++)
+    {
+        // counts letters
+        if (((int)ext[i] > 64 && (int)ext[i] < 91) || ((int)ext[i] > 96 && (int)ext[i] < 123))
+        {
+            totletter++;
+        }
+        //counts spaces
+        if ((int)ext[i] == 32)
+        {
+            totword++;
+            subword++;
+        }
+        //counts full stops, exclamation marks and question marks
+        if ((int)ext[i] == 46 || (int)ext[i] == 33 || (int)ext[i] == 63)
+        {
+            totsen++;
+            subsen++;
+            subsen2 = subsen;
+            if((subsen2 == 1 && subword == 1) || (subsen2 == 1 && subword == 0))
+            {
+                redsen++;
+            }
+            subsen = 0;
+            subword = 0;
+        }
+    }
+
+    totsen -= redsen;
+
+    double s, l, grade;
+    //l is no of letters per 100 word
+    l = (totletter / totword) * 100 ;
+    //s is no of sentences per 100 words
+    s = (totsen / totword) * 100;
+    //englis US grade defined as by Coleman-Liau.
+    grade = (0.0588 * l) - (0.296 * s) - 15.8;
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade > 15)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %d\n", (int)round(grade));
+    }
+}
